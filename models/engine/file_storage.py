efedaaa0187746c6517b7ca4c95b89f50fa821ce
David-Inkheart <okoliedave1@gmail.com>
2023-01-04 12:22:28 -0800

    adding "delete" method and updating "all" method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        new_dict = {}
+        if cls is None:
+            return self.__objects
+        for key, val in self.__objects.items():
+            if type(val) == cls:
+                new_dict[key] = val
+        return new_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +30,19 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """
+        deletes obj from __objects if it’s inside
+        if obj is equal to None,
+        the method should not do anything
+        """
+        if obj is None:
+            return
+        key = obj.__class__.__name__ + '.' + obj.id
+        if key in self.__objects:
+            del self.__objects[key]
+        self.save()
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -45,6 +64,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
