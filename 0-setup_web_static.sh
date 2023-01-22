1a96d7dba1452b8364521bf8a6c8a047db3a0a98
rotex <dantejake48@gmail.com>
2023-01-22 19:47:25 +0100

    ficing config file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,20 +7,21 @@ dpkg -s nginx 2>/dev/null >/dev/null || sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
+echo "Hello World" | tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/  /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-input="\
-server {
-	location /hbnb_static/ {
-		alias /data/web_static/current/;
-		autoindex off;
-	}
-}
-"
-echo "$input" >> /etc/nginx/sites-available/default
+#input="\
+#server {
+#	location /hbnb_static/ {
+#		alias /data/web_static/current/;
+#		autoindex off;
+#	}
+#}
+#"
+#echo "$input" >> /etc/nginx/sites-available/default
+sudo sed -i '39i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
