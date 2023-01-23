f657e2091ed9ff0fa170da6d529208aac1ed12af
rotex <dantejake48@gmail.com>
2023-01-23 09:07:43 +0100

    removed all sudo permission

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,21 +20,21 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, "/tmp/{}".format(filename_ext))
-        run("sudo mkdir -p /data/web_static/releases/{}".format(filename))
+        run("mkdir -p /data/web_static/releases/{}".format(filename))
 
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(filename_ext, filename))
 
-        run("sudo rm -f /tmp/{}".format(filename_ext))
+        run("rm -f /tmp/{}".format(filename_ext))
 
-        run("sudo mv /data/web_static/releases/{}/web_static/* \
+        run("mv /data/web_static/releases/{}/web_static/* \
                 /data/web_static/releases/{}/".format(filename, filename))
 
-        run("sudo rm -rf /data/web_static/releases/{}/web_static"
+        run("rm -rf /data/web_static/releases/{}/web_static"
             .format(filename))
 
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/  \
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/  \
                 /data/web_static/current".format(filename))
         print("New version deployed!")
         return True
