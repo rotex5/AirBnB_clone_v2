5a1e45c5beb05562125b5400e411f95e009436ff
rotex <dantejake48@gmail.com>
2023-01-23 08:28:49 +0100

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -42,13 +42,22 @@ def do_deploy(archive_path):
     filename_ext = archive_path.split("/")[-1]
 
     try:
-        put(archive_path, "/tmp/")
+        put(archive_path, "/tmp/{}".format(filename_ext))
         run("sudo mkdir -p /data/web_static/releases/{}".format(filename))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(filename_ext, filename))
+
         run("sudo rm -f /tmp/{}".format(filename_ext))
-        run("sudo rm -f /data/web_static/current")
-        run("sudo ln -sf /data/web_static/releases/{}  \
+
+        run("sudo mv /data/web_static/releases/{}/web_static/* \
+                /data/web_static/releases/{}/".format(filename, filename))
+
+        run("sudo rm -rf /data/web_static/releases/{}/web_static"
+            .format(filename))
+
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/  \
                 /data/web_static/current".format(filename))
         print("New version deployed!")
         return True
