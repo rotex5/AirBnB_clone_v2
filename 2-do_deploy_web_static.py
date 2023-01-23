4b50a20b9e55c2a86eabb792bbbb1bb1af484e99
rotex <dantejake48@gmail.com>
2023-01-23 09:01:33 +0100

    deleted do_pack method

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,32 +4,9 @@ that distributes an archive to your web servers
 """
 from fabric.api import *
 from os import path
-import time
 
 env.hosts = ["54.172.37.10", "54.82.176.244"]
 env.user = "ubuntu"
-
-
-def do_pack():
-    """
-    generates a .tgz archive from
-    the contents of the web_static folder
-    """
-    try:
-        local("mkdir -p versions")
-
-        t = time.strftime("%Y%m%d%H%M%S")
-        gen_path = "versions/web_static_{}.tgz".format(t)
-
-        local("tar -cvzf {} web_static".format(gen_path))
-
-        get_size = local('wc -c {}'.format(gen_path), capture=True)
-        get_size = get_size.split(" ")[0]
-
-        print("web_static packed: {} -> {}Bytes".format(gen_path, get_size))
-        return gen_path
-    except Exception:
-        return None
 
 
 def do_deploy(archive_path):
