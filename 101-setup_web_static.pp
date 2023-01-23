58b1ddd83c2da302a3e9d08ccfa3a150264214aa
rotex <dantejake48@gmail.com>
2023-01-23 19:27:49 +0100

    test1

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,62 +1,64 @@
 # puppet script that sets up your web servers for the deployment of web_static
 
-exec {'update':
-  provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
+exec { '/usr/bin/env apt -y update' : }
+
+package { 'nginx':
+  ensure => installed,
 }
 
-exec {'install Nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['start Nginx'],
+file { '/data':
+  ensure  => 'directory'
 }
 
-exec {'start Nginx':
-  provider => shell,
-  command  => 'sudo service nginx start',
-  before   => Exec['create first directory'],
+file { '/data/web_static':
+  ensure => 'directory'
 }
 
-exec {'create first directory':
-  provider => shell,
-  command  => 'sudo mkdir -p /data/web_static/releases/test/',
-  before   => Exec['create second directory'],
+file { '/data/web_static/releases':
+  ensure => 'directory'
 }
 
-exec {'create second directory':
-  provider => shell,
-  command  => 'sudo mkdir -p /data/web_static/shared/',
-  before   => Exec['content into html'],
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
 }
 
-exec {'content into html':
-  provider => shell,
-  command  => 'echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html',
-  before   => Exec['symbolic link'],
+file { '/data/web_static/shared':
+  ensure => 'directory'
 }
 
-exec {'symbolic link':
-  provider => shell,
-  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
-  before   => Exec['put location'],
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => 'Hello world'
 }
 
-exec {'put location':
-  provider => shell,
-  command  => 'sudo sed -i \'38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\' /etc/nginx/sites-available/default',
-  before   => Exec['restart Nginx'],
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
 }
 
-exec {'restart Nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
-  before   => File['/data/']
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-file {'/data/':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  recurse => true,
+file { '/var/www':
+  ensure => 'directory'
+}
+
+file { '/var/www/html':
+  ensure => 'directory'
+}
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => 'Hello World'
+}
+
+exec { 'nginx_conf':
+  environment => ['data=\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n'],
+  command     => 'sed -i "39i $data" /etc/nginx/sites-enabled/default',
+  path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin'
+}
+
+service { 'nginx':
+  ensure => running,
 }
