0ddb74f148f12b6a1f5fb32c9cf638a0767b39f7
rotex <dantejake48@gmail.com>
2023-01-23 19:01:08 +0100

    fix

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,32 +1,48 @@
 # puppet script that sets up your web servers for the deployment of web_static
-include stdlib
 
-exec { 'update packages':
-  command => '/usr/bin/apt-get update'
+exec { 'update':
+  command => 'sudo apt-get -y update',
+  provier => shell,
 }
 
 package { 'nginx':
-  ensure  => installed,
-  require => Exec['update packages']
+  ensure => 'installed',
 }
 
-exec {'create directories':
-  command  => '/usr/bin/mkdir -p /data/web_static/releases/test/',
+service { 'ngnix':
+  ensure  => 'running',
+  require => Package['nginx'],
 }
 
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'present',
-  content => 'Hello World!',
+exec {'create first directory':
+  command  => 'sudo mkdir -p /data/web_static/releases/test/',
+  provider => shell,
+  before   => Exec['create second directory'],
 }
 
-file { '/data/web_static/shared':
-  ensure  => 'directory'
-  require => Exec['create directories']
+exec {'create second directory':
+  command  => 'sudo mkdir -p /data/web_static/shared/',
+  provider => shell,
+  before   => Exec['content into html'],
+}
+
+exec {'content into html':
+  command  => 'echo "Hello World" | sudo tee /data/web_static/releases/test/index.html',
+  provider => shell,
+  before   => Exec['symbolic link'],
 }
 
 file { '/data/web_static/current':
   ensure => 'link',
-  target => '/data/web_static/releases/test'
+  target => /data/web_static/releases/test/,
+  before => Exec['nginx config'],
+}
+
+exec { 'nginx config':
+  environment => ['input=\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n'],
+  command     => 'sed -i "39i $input" /etc/nginx/sites-enabled/default',
+  path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin'
+  before      => File['/data/']
 }
 
 file {'/data/':
@@ -34,15 +50,10 @@ file {'/data/':
   owner   => 'ubuntu',
   group   => 'ubuntu',
   recurse => true,
+  before  => Service['nginx']
 }
 
-exec { 'nginx_conf':
-  environment => ['data=\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n'],
-  command     => 'sed -i "39i $data" /etc/nginx/sites-enabled/default',
-  path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin'
-}
-
-exec { 'restart nginix':
-  command => '/usr/sbin/service nginx restart',
-  require => Package['nginx']
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
 }
