3a72aca15bcc9f277b77781bb188fcd9d8aca328
rotex <dantejake48@gmail.com>
2023-02-04 00:58:49 +0100

    display list of States  or a detail about a State based on route

---
+++ web_flask/9-states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Flaskweb application for HBNB project """
+
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db_conn(exception):
+    """teardown session connection to db"""
+    storage.close()
+
+
+@app.route('/states/', strict_slashes=False)
+def list_states():
+    """ passes a list state
+    to /states route"""
+    states = storage.all(State).values()
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_detail(id):
+    """ passes a details of a state
+    to /states/id route"""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
