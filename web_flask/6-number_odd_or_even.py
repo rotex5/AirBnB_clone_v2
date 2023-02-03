17234033f982878aedf696f254d64e7d663a5068
rotex <dantejake48@gmail.com>
2023-02-03 13:56:41 +0100

    even or odd

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+""" Flaskweb application for HBNB project """
+
+from flask import Flask
+from flask import render_template
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ display value at root path """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """ """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """ display C + text passed"""
+    return 'C {}'.format(escape(text.replace('_', ' ')))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text='is cool'):
+    """ display Python + text passed """
+    if not text:
+        text = 'is cool'
+    return 'Python {}'.format(escape(text.replace('_', ' ')))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_num(n):
+    """ display number if its an interger """
+    return '{:d} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_template(n):
+    """ display a HTML page only if n is an integer """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def even_odd(n):
+    """ display a HTML page only if n is an integer """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
